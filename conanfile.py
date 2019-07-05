19d1ec3143ab96c964f5f6671bfafed087f0e8f5
SSE4 <tomskside@gmail.com>
2019-07-05 11:03:02 +0200

    - aplpy bincrafters conventions
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ class ReadLineConan(ConanFile):
     homepage = "https://tiswww.case.edu/php/chet/readline/rltop.html"
     topics = ("conan", "readline", "cli", "terminal", "command")
     author = "Bincrafters <bincrafters@gmail.com>"
-    license = "GPL-3"
+    license = "GPL-3.0-only"
     exports = ["LICENSE.md"]
     exports_sources = ["readline_mingw.patch"]
     settings = "os", "arch", "compiler", "build_type"
