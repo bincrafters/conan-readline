375e4402fdeb1c436163d77591fa8a316931d304
Julien Marrec <julien.marrec@gmail.com>
2019-08-28 12:45:30 +0200

    Forgot to check and adjust the build.py file... [skip appveyor]

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
-    shared_option_name = False if platform.system() == "Darwin" else "readline:shared"
-    builder = build_template_default.get_builder(pure_c=True, shared_option_name=shared_option_name)
+    shared_option_name = "readline:shared"
+    builder = build_template_default.get_builder(pure_c=True,
+                                                 shared_option_name=shared_option_name)
     builder.run()
